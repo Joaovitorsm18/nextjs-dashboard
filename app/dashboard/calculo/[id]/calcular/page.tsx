ab0dd8693127eeda4baf36f494b2dc979f82c9b7
João Vítor <jaovit81@gmail.com>
2024-06-23 23:05:07 -0300

    calcular

---
+++ app/dashboard/calculo/[id]/calcular/page.tsx
@@ -0,0 +1,32 @@
+import Form from '@/app/ui/calculo/calculo-form';
+import Breadcrumbs from '@/app/ui/calculo/breadcrumbs';
+import { fetchCondominiosById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
+    const [condominios] = await Promise.all([
+        fetchCondominiosById(id),
+
+    ]);
+
+    if (!condominios) {
+        notFound();
+    }
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Condominios', href: '/dashboard/calculo' },
+                    {
+                        label: 'Calcular',
+                        href: `/dashboard/calculo/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form condominios={condominios} />
+        </main>
+    );
+}
