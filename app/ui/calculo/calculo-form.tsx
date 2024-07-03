466bb964adcd4b5521ce51a712a48b0d3008e0a7
João Vítor <jaovit81@gmail.com>
2024-07-03 19:38:15 -0300

    alteração para teste

--- app/ui/calculo/calculo-form.tsx
+++ app/ui/calculo/calculo-form.tsx
@@ -31,7 +31,7 @@ export default function EditCondominioForm({
     };
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      const response = await fetch('https://back-end-6hjm.onrender.com/process-login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
