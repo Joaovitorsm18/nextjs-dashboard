c5df372ea982b6f838ea73b7f26fe5ed99a05f3a
João Vítor <jaovit81@gmail.com>
2024-07-14 20:42:05 -0300

    Criação de modal para detalhes

--- app/ui/calculo/Modal.tsx
+++ app/ui/calculo/Modal.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 
-interface ModalProps {
+interface ModalDetalhesProps {
     isOpen: boolean;
     onClose: () => void;
     children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const ModalDetalhes: React.FC<ModalDetalhesProps> = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
-            <div className="relative bg-white rounded-lg shadow-lg w-full max-w-4xl mx-auto p-4">
-                <button
-                    onClick={onClose}
-                    className="absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700"
-                >
-                    &times;
-                </button>
-                {children}
+            <div className="relative bg-white rounded-lg shadow-lg w-full max-w-7xl mx-auto p-4">
+                <div className="max-h-screen overflow-y-auto">
+                    {children}
+                </div>
+                <div className="flex justify-end mt-4">
+                    <button
+                        onClick={onClose}
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400"
+                    >
+                        Fechar
+                    </button>
+                </div>
             </div>
         </div>
     );
 };
 
-export default Modal;
+export default ModalDetalhes;
--- app/ui/calculo/calculo-form.tsx
+++ app/ui/calculo/calculo-form.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { useState, useEffect, useRef } from 'react';
 import Modal from './modal-erro';
+import ModalDetalhes from './Modal';
+import { EyeIcon } from '@heroicons/react/24/outline';
 
 export default function EditCondominioForm({
   condominios,
@@ -14,6 +16,7 @@ export default function EditCondominioForm({
   const [resultados, setResultados] = useState<Resultado | null>(null);
   const [error, setError] = useState<string | null>(null); // Estado para o erro
   const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar o modal
+  const [isModalDetalhesOpen, setIsModalDetalhesOpen] = useState(false); // Estado para controlar o modal
   const resultRef = useRef<HTMLDivElement | null>(null);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, condominios: CondominioForm) => {
@@ -36,8 +39,8 @@ export default function EditCondominioForm({
     };
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
-        //const response = await fetch('http://localhost:3001/process-login', {
+      //const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+      const response = await fetch('http://localhost:3001/process-login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -222,6 +225,14 @@ export default function EditCondominioForm({
       {resultados && !error && (
         <div ref={resultRef} className="mt-8 rounded-md bg-gray-50 p-6 shadow-md">
           <h2 className="text-xl font-semibold mb-6 text-center">Resultados</h2>
+          <div className="flex justify-end">
+            <button
+              className="rounded-md border p-2 hover:bg-gray-100"
+              onClick={() => setIsModalDetalhesOpen(true)}
+            >
+              <EyeIcon className="w-4" />
+            </button>
+          </div>
           <div className="overflow-x-auto">
             <table className="w-full max-w-3xl mx-auto border-collapse rounded-md overflow-hidden shadow-2xl">
               <thead className="bg-blue-600 text-white">
@@ -321,8 +332,173 @@ export default function EditCondominioForm({
               Voltar
             </Link>
             <Link href={`/dashboard/calculo/${condominios.id}/calcular`} className="flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">Calcular Novamente</Link>
+            <button
+              type="button"
+              onClick={() => setIsModalDetalhesOpen(true)}
+              className="flex h-10 items-center rounded-lg bg-green-500 px-4 text-sm font-medium text-white transition-colors hover:bg-green-400"
+            >
+              Salvar
+            </button>
           </div>
+
+          <ModalDetalhes isOpen={isModalDetalhesOpen} onClose={() => setIsModalDetalhesOpen(false)}>
+            <h2 className="text-xl font-semibold mt-8 mb-6 text-center">Cobrança por Unidades</h2>
+            <div className="overflow-x-auto">
+              <table className="w-full max-w-4xl mx-auto border-collapse rounded-md overflow-hidden shadow-2xl">
+                <thead className="bg-green-600 text-white">
+                  <tr>
+                    <th className="px-4 py-2 border-r w-1/12" rowSpan={2}>Apartamento</th>
+                    <th className="px-4 py-2 border-r w-1/12" rowSpan={2}>Consumos</th>
+                    <th className="px-4 py-2 border-r w-1/12" rowSpan={2}>Taxa Fixa</th>
+                    {resultados.consumoApartamentosPrimeirafaixa && (
+                      <th className="px-4 py-2 border-r w-1/6" colSpan={2}>Primeira faixa</th>
+                    )}
+                    {resultados.consumoApartamentosSegundafaixa && (
+                      <th className="px-4 py-2 border-r w-1/6" colSpan={2}>Segunda faixa</th>
+                    )}
+                    {resultados.consumoApartamentosTerceirafaixa && (
+                      <th className="px-4 py-2 border-r w-1/6" colSpan={2}>Terceira faixa</th>
+                    )}
+                    {resultados.consumoApartamentosQuartafaixa && (
+                      <th className="px-4 py-2 border-r w-1/6" colSpan={2}>Quarta faixa</th>
+                    )}
+                    {resultados.consumoApartamentosQuintafaixa && (
+                      <th className="px-4 py-2 border-r w-1/6" colSpan={2}>Quinta faixa</th>
+                    )}
+                    {resultados.valorCondominio && (
+                      <th className="px-4 py-2 border-r w-1/12" rowSpan={2}>Condomínio</th>
+                    )}
+                    <th className="px-4 py-2 w-1/12" rowSpan={2}>Valor_Final</th>
+                  </tr>
+                  <tr>
+                    {resultados.consumoApartamentosPrimeirafaixa && (
+                      <>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Consumo</th>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Resultado</th>
+                      </>
+                    )}
+                    {resultados.consumoApartamentosSegundafaixa && (
+                      <>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Consumo</th>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Resultado</th>
+                      </>
+                    )}
+                    {resultados.consumoApartamentosTerceirafaixa && (
+                      <>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Consumo</th>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Resultado</th>
+                      </>
+                    )}
+                    {resultados.consumoApartamentosQuartafaixa && (
+                      <>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Consumo</th>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Resultado</th>
+                      </>
+                    )}
+                    {resultados.consumoApartamentosQuintafaixa && (
+                      <>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Consumo</th>
+                        <th className="bg-blue-600 px-4 py-2 border w-1/12">Resultado</th>
+                      </>
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {resultados.cobrancaPorApartamento.map((apartamento, index) => (
+                    <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : ""}>
+                      <td className="border px-4 py-2 border-r">{Object.keys(apartamento)[0]}</td>
+                      <td className="border px-4 py-2">{resultados.consumosApartamentos[index]}</td>
+                      <td className="border px-4 py-2">{resultados.taxaFixa}</td>
+                      {resultados.consumoApartamentosPrimeirafaixa && (
+                        <>
+                          <td className="border px-4 py-2">{resultados.consumoApartamentosPrimeirafaixa[index]}</td>
+                          <td className="border px-4 py-2">{resultados.resultadoApartamentosPrimeirafaixa[index]}</td>
+                        </>
+                      )}
+                      {resultados.consumoApartamentosSegundafaixa && (
+                        <>
+                          <td className="border px-4 py-2">{resultados.consumoApartamentosSegundafaixa[index]}</td>
+                          <td className="border px-4 py-2">{resultados.resultadoApartamentosSegundafaixa[index]}</td>
+                        </>
+                      )}
+                      {resultados.consumoApartamentosTerceirafaixa && (
+                        <>
+                          <td className="border px-4 py-2">{resultados.consumoApartamentosTerceirafaixa[index]}</td>
+                          <td className="border px-4 py-2">{resultados.resultadoApartamentosTerceirafaixa[index]}</td>
+                        </>
+                      )}
+                      {resultados.consumoApartamentosQuartafaixa && (
+                        <>
+                          <td className="border px-4 py-2">{resultados.consumoApartamentosQuartafaixa[index]}</td>
+                          <td className="border px-4 py-2">{resultados.resultadoApartamentosQuartafaixa[index]}</td>
+                        </>
+                      )}
+                      {resultados.consumoApartamentosQuintafaixa && (
+                        <>
+                          <td className="border px-4 py-2">{resultados.consumoApartamentosQuintafaixa[index]}</td>
+                          <td className="border px-4 py-2">{resultados.resultadoApartamentosQuintafaixa[index]}</td>
+                        </>
+                      )}
+                      {resultados.valorCondominio && (
+                        <>
+                          <td className="border px-4 py-2">{`R$ ` + resultados.valorCondominio}</td>
+                        </>
+                      )}
+                      <td className="border px-4 py-2">{Object.values(apartamento)[0]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+                {resultados.cobrancaPorLoja && (
+                  <>
+                    <tbody>
+                      {resultados.cobrancaPorLoja.map((loja, index) => (
+                        <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : ""}>
+                          <td className="border px-4 py-2 border-r">{Object.keys(loja)[0]}</td>
+                          <td className="border px-4 py-2">{resultados.consumosLoja[index]}</td>
+                          <td className="border px-4 py-2">{resultados.taxaFixaLoja}</td>
+                          {resultados.consumoLojaPrimeirafaixa && (
+                            <>
+                              <td className="border px-4 py-2">{resultados.consumoLojaPrimeirafaixa[index]}</td>
+                              <td className="border px-4 py-2">{resultados.resultadoLojaPrimeirafaixa[index]}</td>
+                            </>
+                          )}
+                          {resultados.consumoLojaSegundafaixa && (
+                            <>
+                              <td className="border px-4 py-2">{resultados.consumoLojaSegundafaixa[index]}</td>
+                              <td className="border px-4 py-2">{resultados.resultadoLojaSegundafaixa[index]}</td>
+                            </>
+                          )}
+                          {resultados.consumoLojaTerceirafaixa && (
+                            <>
+                              <td className="border px-4 py-2">{resultados.consumoLojaTerceirafaixa[index]}</td>
+                              <td className="border px-4 py-2">{resultados.resultadoLojaTerceirafaixa[index]}</td>
+                            </>
+                          )}
+                          {resultados.consumoLojaQuartafaixa && (
+                            <>
+                              <td className="border px-4 py-2">{resultados.consumoLojaQuartafaixa[index]}</td>
+                              <td className="border px-4 py-2">{resultados.resultadoLojaQuartafaixa[index]}</td>
+                            </>
+                          )}
+                          {resultados.consumoLojaQuintafaixa && (
+                            <>
+                              <td className="border px-4 py-2">{resultados.consumoLojaQuintafaixa[index]}</td>
+                              <td className="border px-4 py-2">{resultados.resultadoLojaQuintafaixa[index]}</td>
+                            </>
+                          )}
+                          <td className="border px-4 py-2"></td>
+                          <td className="border px-4 py-2">{Object.values(loja)[0]}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </>
+                )}
+
+              </table>
+            </div>
+          </ModalDetalhes>
         </div>
+
       )}
     </form>
   );
